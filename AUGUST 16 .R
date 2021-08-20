b7fd6211550ea37e15f6d1d8f7a8bda6c568c44d
Esther Chukwunwike <84766550+mmeli19@users.noreply.github.com>
2021-08-20 01:14:35 -0500

    Add files via upload
    Calculate stdev, Median values.

---
+++ AUGUST 16 .R
@@ -0,0 +1,13 @@
+#Author: Esther Chukwunwike, Date: Aug 16, 2021, Purpose: Calculate stdev, Median values.
+
+# Calculate standard deviation and standard deviation. 
+
+# A variable to hold 7 numbers.
+variable1 <- c(1, 2, 3, 4, 5, 6, 7)
+
+sd(variable1)
+# 2.160247
+
+# Calculate median. 
+median(variable1)
+#4
