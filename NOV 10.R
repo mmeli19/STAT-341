77198bde8d7307f8ef0967bab11adce17afc1c15
Esther Chukwunwike <84766550+mmeli19@users.noreply.github.com>
2021-11-14 14:50:57 -0500

    Update NOV 10.R

--- NOV 10.R
+++ NOV 10.R
@@ -6,12 +6,12 @@ data_frame <- read.csv("https://goo.gl/j6lRXD")
 # Observe the data to check if there is any relationship between treatment and outcomes?
 table(data_frame$treatment, data_frame$improvement)
 # improved not-improved
-  not-treated       26           29
-  treated           35           15
+# not-treated       26           29
+# treated           35           15
 
 
 # Perform the Chi-Square test
 chisq.test(data_frame$treatment, data_frame$improvement, correct=FALSE)
 # Pearson's Chi-squared test
-  data:  data_frame$treatment and data_frame$improvement
-  X-squared = 5.5569, df = 1, p-value = 0.01841
+# data:  data_frame$treatment and data_frame$improvement
+# X-squared = 5.5569, df = 1, p-value = 0.01841
