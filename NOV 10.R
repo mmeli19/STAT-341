673a96c1cdf5ecdc870543f7bd8ef0b1a949912d
Esther Chukwunwike <84766550+mmeli19@users.noreply.github.com>
2021-11-14 14:50:11 -0500

    Add files via upload

---
+++ NOV 10.R
@@ -0,0 +1,17 @@
+# Author: Esther Chukwunwike, Date: Nov 10, 2021, Purpose: Calculate Chi-Square value on sample data
+
+# read the data from link
+data_frame <- read.csv("https://goo.gl/j6lRXD")
+
+# Observe the data to check if there is any relationship between treatment and outcomes?
+table(data_frame$treatment, data_frame$improvement)
+# improved not-improved
+  not-treated       26           29
+  treated           35           15
+
+
+# Perform the Chi-Square test
+chisq.test(data_frame$treatment, data_frame$improvement, correct=FALSE)
+# Pearson's Chi-squared test
+  data:  data_frame$treatment and data_frame$improvement
+  X-squared = 5.5569, df = 1, p-value = 0.01841
